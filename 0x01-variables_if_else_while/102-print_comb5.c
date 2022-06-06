9d2be19216e6064da323fc152baf1cb3d8884af5
Ivie Osoiye <ivieosoiye@gmail.com>
2022-06-06 14:20:40 -0700

    created file 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of two-digit numbers
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int d;
+	int c;
+	int b;
+	int a;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		for (c = '0'; c <= '9'; c++)
+		{
+			for (b = d; b <= '9'; b++)
+			{
+				putchar(d);
+				putchar(c);
+				putchar(' ');
+				putchar(b);
+				putchar(a);
+
+				if (!((d == '9' && c == '8') && (b == '9' && a == '9')))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			a = '0';
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
