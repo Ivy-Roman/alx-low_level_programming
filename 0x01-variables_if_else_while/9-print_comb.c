8b8ae8c83ca2e623fb75a22e73e16d2843b24b4d
Ivie Osoiye <ivieosoiye@gmail.com>
2022-06-06 12:43:10 -0700

    created file 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print 0-9 separated by comma
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
