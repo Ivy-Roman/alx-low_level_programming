b373a82ca06f145c62ac076e2831f42d22d22bc5
Ivie Osoiye <ivieosoiye@gmail.com>
2022-06-08 07:21:23 -0700

    edited code

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,15 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n *= -1;
-
-	_putchar('0' + (n % 10));
-
-	return (n % 10);
+	if (n > 0 || n == 0)
+	{
+		_putchar('0' + (n % 10));
+		return (n % 10);
+	}
+	else
+	{
+		n = n * -1;
+		_putchar('0' + (n % 10));
+		return (n % 10);
+	}
 }
