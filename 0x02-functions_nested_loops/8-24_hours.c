a381038c18f32302f2633144dafa28b26001eab2
Ivie Osoiye <ivieosoiye@gmail.com>
2022-06-08 11:32:00 -0700

    edited the code

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,9 +10,9 @@ void jack_bauer(void)
 	int hours = 0;
 	int min = 0;
 
-	if (hours < 24)
+	while (hours < 24)
 	{
-		if (min < 60)
+		while (min < 60)
 		{
 			_putchar('0' + (hours / 10));
 			_putchar('0' + (hours % 10));
@@ -22,6 +22,7 @@ void jack_bauer(void)
 			_putchar('\n');
 			min++;
 		}
+		min = 0;
 		hours++;
 	}
 }
