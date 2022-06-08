997b89fa9120d0ffefcbc65bf90cb5c8ea2aba14
Ivie Osoiye <ivieosoiye@gmail.com>
2022-06-08 12:39:21 -0700

    corrected betty errors

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * add(int, int) -  adds two integers and returns the result
- * @a - first argument
- * @b - second argument
- * Return: always 0
+ * add -  adds two integers and returns the result
+ * @a: first argument
+ * @b: second argument
+ * Return: sum
  */
 
 int add(int a, int b)
