2b12dbb7f5961b484dc90d391b479cec65582637
Ivie Osoiye <ivieosoiye@gmail.com>
2022-06-13 15:24:01 -0700

    declared a function

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @*a: the first value to be swapped
+ * @*b: the second value to be swapped
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
