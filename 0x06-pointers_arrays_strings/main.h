c867f44d4ee2f604cbdfe8148a2b296c92cbd8fd
Ivie Osoiye <ivieosoiye@gmail.com>
2022-06-16 00:54:59 -0700

    added a prototype to main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,84 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately
+ */
+
+int _putchar(char c);
+
+/**
+ * *_strcat(char *dest, char *src) - concatenates two strings.
+ */
+
+char *_strcat(char *dest, char *src);
+
+
+/**
+ * swap_int(int *a, int *b) - swaps the values of two integers
+ */
+
+void swap_int(int *a, int *b);
+
+/**
+ * _strlen(char *s) -  returns the length of a string.
+ */
+
+int _strlen(char *s);
+
+/**
+ * _puts(char *str) -  prints a string, followed by a new line,
+ * to standard output.
+ */
+
+void _puts(char *str);
+
+/**
+ * print_rev(char *s) - prints a string, in reverse
+ */
+
+void print_rev(char *s);
+
+/**
+ * rev_string(char *s) - reverses a string
+ */
+
+void rev_string(char *s);
+
+/**
+ * puts2(char *str) - prints every other character of a string
+ */
+
+void puts2(char *str);
+
+/**
+ * puts_half(char *str) - prints half of a string
+ */
+
+void puts_half(char *str);
+
+/**
+ * print_array(int *a, int n) - prints n elements of an array
+ * of integers
+ */
+
+void print_array(int *a, int n);
+
+/**
+ * *_strcpy(char *dest, char *src) - copies the string
+ * pointed to by src
+ */
+
+char *_strcpy(char *dest, char *src);
+
+/**
+ * _atoi(char *s) - convert a string to an integer
+ */
+
+int _atoi(char *s);
+
+#endif
